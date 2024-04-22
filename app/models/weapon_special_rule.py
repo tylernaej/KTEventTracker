3c872fb5d52349a44846b5341fa70bcb3b40454e
tyler <tylernaej@gmail.com>
2024-04-22 18:44:09 -0400

    weapon rules added

---
+++ app/models/weapon_special_rule.py
@@ -0,0 +1,10 @@
+from .db import db
+
+class Weapon_Special_Rule(db.Model):
+    __tablename__ = 'weapon_rules'
+
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(100), nullable=False)
+
+    def __repr__(self):
+        return f'<{self.name} is a weapon special rule>'
